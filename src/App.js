992c8ea0df2ca65b22997f26e0ed2dfa9838a835
Luce <boama561@gmail.com>
2025-05-02 12:32:22 +0000

    Ajout boutton.js et css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Button from './components/Button';
 import Counter from './components/Counter';
+
 function App() {
 
   const [count, setCount] = useState(0);
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import './Button.css';
 export default function Button({content, click}){
-    return  <button onClick={click}>{content}</button>
+    return  <button className='button' onClick={click}>{content}</button>
 }
